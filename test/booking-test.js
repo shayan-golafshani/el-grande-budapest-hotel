a3fcad9726f28a39378979959d61681b36118609
Shayan <70605985+shayan-golafshani@users.noreply.github.com>
2021-06-16 13:36:28 -0600

    Fix import on bookings test

--- test/booking-test.js
+++ test/booking-test.js
@@ -1,25 +1,37 @@
 import chai from 'chai';
 import { Booking } from '../src/classes/Booking';
-import { allBookings } from '../src/test-data/booking-data';
-const expect = chai.expect;
+import { bookings } from '../src/test-data/booking-data';
 
+const expect = chai.expect;
 
 describe('Booking', () => {
   let booking1;
   beforeEach(() => { 
-    booking1 = new Booking(allBookings[0]);
+    booking1 = new Booking(bookings[0]);
   });
 
   it('Should be a function', () => {
     expect(Booking).to.be.a('function');
   });
 
-  it('Should store the values of the booking object that\'s passed in', () => {    
-    expect(booking1.id).to.equal(allBookings[0].id);
-    expect(booking1.userID).to.equal(allBookings[0].userID);
-    expect(booking1.date).to.equal(allBookings[0].date);
-    expect(booking1.roomNumber).to.equal(allBookings[0].roomNumber);
-    expect(booking1.roomServiceCharges).to.equal(allBookings[0].roomServiceCharges);
+  it('Should store the values of the booking ID ', () => {    
+    expect(booking1.id).to.equal(bookings[0].id);
+  });
+
+  it("Should store the associate user ID", () => {
+    expect(booking1.userID).to.equal(bookings[0].userID);
+  });
+
+  it("Should store the booking date", () => {
+    expect(booking1.date).to.equal(bookings[0].date);
+  });
+
+  it("Should store the booking room number", () => {
+    expect(booking1.roomNumber).to.equal(bookings[0].roomNumber);
+  });
+
+  it("Should store the associated booking's room service charges", () => {
+    expect(booking1.roomServiceCharges).to.equal(bookings[0].roomServiceCharges);
   });
 
   it("Should have a method to add room service charges!", () => {
